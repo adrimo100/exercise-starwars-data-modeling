d513ce23fd727c26dfaa06f43dae1105f83d46d6
Adrián Molina Elvira <adrimo100@gmail.com>
2022-02-28 20:43:37 +0000

    Primera Version

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,51 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(20), unique = True, nullable=False)
+    description = Column(String(200))
+    birth = Column(DateTime, nullable=False)
+    eye_color = Column(String(10))
+    skin_color = Column(String(10))
+    height = Column(Float, nullable = False)
+    natal_planet = Column(Integer, ForeignKey("planet.id"), nullable = False )
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    
+
+class Planet(Base):
+
+    __tablename__ = "planet"
+
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(20), unique = True, nullable=False)
+    description = Column(String(200))
+    population = Column(Integer, nullable = False)
+    climate = Column(Integer, nullable = False)
+    orbital_period = Column(Integer)
+    rotation_period = Column(Integer)
+    diameter = Column(Integer, nullable = False)
 
-    def to_dict(self):
-        return {}
+
+     
+
+class Ship(Base):
+     
+    __tablename__ = "ship"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), unique = True, nullable=False)
+    description = Column(String(200))
+    atmosphere_speed = Column(Float, nullable = False)
+    void_speed = Column(Float)
+    hyperspace_class = Column(Float)
+    pilot = Column(Float, ForeignKey("character.id"))
+
+     
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
